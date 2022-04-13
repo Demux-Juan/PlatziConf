60dd4530ebab740c80492f77c3ece0f87b0cc2a6
juan <juan_mendoza_medina@hotmail.com>
2022-04-13 14:50:36 -0500

    se corrigen problemas con el firestore

--- app/src/main/java/com/example/platziconf/MainActivity.kt
+++ app/src/main/java/com/example/platziconf/MainActivity.kt
@@ -2,22 +2,35 @@ package com.example.platziconf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.os.bundleOf
 import com.example.platziconf.model.Conference
 import com.example.platziconf.model.Speaker
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.firestore.FirebaseFirestore
 import org.json.JSONArray
 import org.json.JSONObject
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val analytics:FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message","Hola mundo")
+        analytics.logEvent("InitScreen",bundle)
 
 
         val jsonArr = JSONArray(
             "[\n" +
                     "  {\n" +
+                    "    'name': 'Juan mendoza', \n" +
                     "    'biography': '##### Biografia 1 #####', \n" +
                     "    'category': 1, \n" +
                     "    'image' : 'https://www.imagehousing.com/image/aYaH1', \n" +
@@ -26,6 +39,7 @@ class MainActivity : AppCompatActivity() {
                     "    'workplace' : '##### Workplace 1 #####'\n" +
                     " },\n " +
                     "  {\n" +
+                    "    'name': 'Brenda jimenez', \n" +
                     "    'biography': '##### Biografia 2 #####', \n" +
                     "    'category': 2, \n" +
                     "    'image' : 'https://www.imagehousing.com/image/TLAjT', \n" +
@@ -34,6 +48,7 @@ class MainActivity : AppCompatActivity() {
                     "    'workplace' : '##### Workplace 2 #####'\n" +
                     " },\n " +
                     "  {\n" +
+                    "    'name': 'Mariel Mendoza', \n" +
                     "    'biography': '##### Biografia 3 #####', \n" +
                     "    'category': 3, \n" +
                     "    'image' : 'https://www.imagehousing.com/image/TFndD', \n" +
@@ -42,6 +57,7 @@ class MainActivity : AppCompatActivity() {
                     "    'workplace' : '##### Workplace 3 #####'\n" +
                     " },\n " +
                     "  {\n" +
+                    "    'name': 'Aaron Nolasco', \n" +
                     "    'biography': '##### Biografia 4 #####', \n" +
                     "    'category': 4, \n" +
                     "    'image' : 'https://www.imagehousing.com/image/c3Xo', \n" +
@@ -50,6 +66,7 @@ class MainActivity : AppCompatActivity() {
                     "    'workplace' : '##### Workplace 4 #####'\n" +
                     " },\n " +
                     "  {\n" +
+                    "    'name': 'Beto Lopez', \n" +
                     "    'biography': '##### Biografia 5 #####', \n" +
                     "    'category': 5, \n" +
                     "    'image' : 'https://www.imagehousing.com/image/cnxQ', \n" +
@@ -58,6 +75,7 @@ class MainActivity : AppCompatActivity() {
                     "    'workplace' : '##### Workplace 5 #####'\n" +
                     " },\n " +
                     "  {\n" +
+                    "    'name': 'Tony Asevedo', \n" +
                     "    'biography': '##### Biografia 6 #####', \n" +
                     "    'category': 6, \n" +
                     "    'image' : 'https://www.imagehousing.com/image/cKli', \n" +
@@ -66,6 +84,7 @@ class MainActivity : AppCompatActivity() {
                     "    'workplace' : '##### Workplace 6 #####'\n" +
                     " },\n " +
                     "  {\n" +
+                    "    'name': 'Rodrigo Salido', \n" +
                     "    'biography': '##### Biografia 7 #####', \n" +
                     "    'category': 7, \n" +
                     "    'image' : 'https://www.imagehousing.com/image/lqim', \n" +
@@ -83,59 +102,62 @@ class MainActivity : AppCompatActivity() {
                     "  {\n" +
                     "    \"datetime\": 1564809000, \n" +
                     "    \"description\": \" *** Descripcion 1 ***\", \n" +
-                    "    \"Speaker\" : \"*** Speaker 1 ***\", \n" +
+                    "    \"speaker\" : \"*** Speaker 1 ***\", \n" +
                     "    \"tag\" : \"tag1\", \n " +
-                    "    \"title\" : \"*** Titutlo 1 **\", \n " +
+                    "    \"title\" : \"*** Titutlo 1 **\" \n " +
                     " },\n " +
                     "  {\n" +
                     "    \"datetime\": 1564809000, \n" +
                     "    \"description\": \" *** Descripcion 2 ***\", \n" +
-                    "    \"Speaker\" : \"*** Speaker 2 ***\", \n" +
+                    "    \"speaker\" : \"*** Speaker 2 ***\", \n" +
                     "    \"tag\" : \"tag2\", \n " +
-                    "    \"title\" : \"*** Titutlo 1 **\", \n " +
+                    "    \"title\" : \"*** Titutlo 1 **\" \n " +
                     " },\n " +
                     "  {\n" +
                     "    \"datetime\": 1564809000, \n" +
                     "    \"description\": \" *** Descripcion 3 ***\", \n" +
-                    "    \"Speaker\" : \"*** Speaker 3 ***\", \n" +
+                    "    \"speaker\" : \"*** Speaker 3 ***\", \n" +
                     "    \"tag\" : \"tag3\", \n " +
-                    "    \"title\" : \"*** Titutlo 3 **\", \n " +
+                    "    \"title\" : \"*** Titutlo 3 **\" \n " +
                     " },\n " +
                     "  {\n" +
                     "    \"datetime\": 1564809000, \n" +
                     "    \"description\": \" *** Descripcion 4 ***\", \n" +
-                    "    \"Speaker\" : \"*** Speaker 4 ***\", \n" +
+                    "    \"speaker\" : \"*** Speaker 4 ***\", \n" +
                     "    \"tag\" : \"tag1\", \n " +
-                    "    \"title\" : \"*** Titutlo 4 **\", \n " +
+                    "    \"title\" : \"*** Titutlo 4 **\" \n " +
                     " },\n " +
                     "  {\n" +
                     "    \"datetime\": 1564809000, \n" +
                     "    \"description\": \" *** Descripcion 5 ***\", \n" +
-                    "    \"Speaker\" : \"*** Speaker 5 ***\", \n" +
+                    "    \"speaker\" : \"*** Speaker 5 ***\", \n" +
                     "    \"tag\" : \"tag2\", \n " +
-                    "    \"title\" : \"*** Titutlo 5 **\", \n " +
+                    "    \"title\" : \"*** Titutlo 5 **\" \n " +
                     " },\n " +
                     "  {\n" +
                     "    \"datetime\": 1564809000, \n" +
                     "    \"description\": \" *** Descripcion 6 ***\", \n" +
-                    "    \"Speaker\" : \"*** Speaker 6 ***\", \n" +
+                    "    \"speaker\" : \"*** Speaker 6 ***\", \n" +
                     "    \"tag\" : \"tag3\", \n " +
-                    "    \"title\" : \"*** Titutlo 6 **\", \n " +
+                    "    \"title\" : \"*** Titutlo 6 **\" \n " +
                     " },\n " +
                     "  {\n" +
                     "    \"datetime\": 1564809000, \n" +
                     "    \"description\": \" *** Descripcion 7 ***\", \n" +
-                    "    \"Speaker\" : \"*** Speaker 7 ***\", \n" +
+                    "    \"speaker\" : \"*** Speaker 7 ***\", \n" +
                     "    \"tag\" : \"tag4\", \n " +
-                    "    \"title\" : \"*** Titutlo 7 **\", \n " +
+                    "    \"title\" : \"*** Titutlo 7 **\" \n " +
                     " }\n" +
                     " ]"
         )
 
 
-        val firebaseFirestore = FirebaseFirestore.getInstance()
+       val firebaseFirestore = FirebaseFirestore.getInstance()
+
+
         for (i in 0 until jsonArr.length()) {
             val aux = jsonArr.get(i) as JSONObject
+            //hace referencia a la clase Speaker
             var speaker = Speaker()
             speaker.name = aux.getString("name")
             speaker.jobtitle = aux.getString("jobtitle")
@@ -151,6 +173,7 @@ class MainActivity : AppCompatActivity() {
 
         for (i in 0 until jsonArr2.length()) {
             val aux = jsonArr2.get(i) as JSONObject
+            //hace referencia a la clase Conference
             var conference = Conference()
 
             val calendario = Calendar.getInstance()
@@ -167,6 +190,14 @@ class MainActivity : AppCompatActivity() {
 
 
         }
+
+
+
+
+
+
+
+
 
 
     }
