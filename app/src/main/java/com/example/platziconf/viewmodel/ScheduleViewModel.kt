09afebf7dd7f9ebde9004e8af5c6f746207b6928
juan <juan_mendoza_medina@hotmail.com>
2022-04-17 00:09:27 -0500

    Se acompletan algunas cosas pero hay error

--- app/src/main/java/com/example/platziconf/viewmodel/ScheduleViewModel.kt
+++ app/src/main/java/com/example/platziconf/viewmodel/ScheduleViewModel.kt
@@ -1,12 +1,14 @@
 package com.example.platziconf.viewmodel
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.platziconf.model.Conference
 import com.example.platziconf.network.Callback
 import com.example.platziconf.network.FirestoreServices
 import java.lang.Exception
 
-class ScheduleViewModel {
+class ScheduleViewModel: ViewModel() {
     //se crea una instancia u objeto llamado firestoreService
     val firestoreService = FirestoreServices()
     var listSchedule: MutableLiveData<List<Conference>> = MutableLiveData()
